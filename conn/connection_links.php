1e3ce37cb0427eec739fe738d66ded684b2dd709
npcabrera13 <neilpaolocabrera@gmail.com>
2026-04-23 07:55:59 +0800

    Fix broken conflict markers in connection_links.php

--- conn/connection_links.php
+++ conn/connection_links.php
@@ -1,34 +1,31 @@
-<!-- Bootstrap -->
+<?php //Bootstrap Links for CSS and JS ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
-<!-- FontAwesome Icons -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-<!-- jQuery (REQUIRED) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<?php //Datatables Links for CSS and JS ?>
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 
-<!-- DataTables v1 -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<!-- DataTables Responsive -->
+<!-- DataTables Core -->
+<link rel="stylesheet" href="https://cdn.datatables.net/2.3.7/css/dataTables.dataTables.css">
+<script src="https://cdn.datatables.net/2.3.7/js/dataTables.js"></script>
+
+<!-- DataTables Extensions (Responsive & Buttons) -->
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
 <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
 
-<!-- Your JS -->
-<script src="../js/usermanagement.js"></script>
-
-<!-- DataTables Buttons Extension -->
-<link rel="stylesheet" href="https://cdn.datatables.net/2.3.7/css/dataTables.dataTables.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.6/css/buttons.dataTables.css">
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdn.datatables.net/2.3.7/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.2.6/js/dataTables.buttons.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.2.6/js/buttons.html5.min.js"></script>
 
-
-<!-- Line Graph -->
+<!-- Chart.js CDN -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<!-- Custom JS -->
+<script src="../js/usersmanagement.js"></script>
